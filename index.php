ff368fe9237fe1f57d53ddb6cdccbed1ca81b8d2
daiki <6280ikiad@gmail.com>
2022-05-06 20:53:11 +0900

    完成

---
+++ index.php
@@ -0,0 +1,25 @@
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<!-- 新着情報 -->
+<div class="wrapper">
+    <div id="news_parent" class="back_ground_color_light_pink visible_960">
+        <div class="back_ground_color_white max_width_920 p_10">
+            <div id="contents">
+                <link rel="stylesheet" href="contents-maker/css/style.css">
+                <p class="">新着情報</p>
+                <div id="news"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="//unpkg.com/vue"></script>
+<script src="contents-maker/js/contents-maker.js" id="contents-maker-js" async></script>
+<script>
+	function show_contents(id){
+        var contents = document.getElementById('input-contents-'+id);
+        contents.style.WebkitLineClamp = '1000';
+    }
+</script>
